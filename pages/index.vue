3a538fad672c33b173783cff28148022cfb28480
Charlotte Fereday <cbrfereday@gmail.com>
2022-01-16 18:02:50 +0000

    feat: fetches data for devTo & Snyk Vulns

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,24 @@
-<script setup>
-const { data } = useFetch('https://dev.to/api/articles?per_page=5&tag=security', {
-    headers: useRequestHeaders(['cookie'])
+<script>
+export default defineComponent({
+  async setup() {
+    const [{ data: devTo}, { data: snyk }] = await Promise.all([
+      useFetch(`https://dev.to/api/articles?per_page=5&tag=security`),
+       useFetch(`https://snyk.io/disclosed-vulnerabilities/data`, { method: 'POST', headers: {'x-requested-with': 'XMLHttpRequest' }}),
+    ])
+    return {
+      devTo,
+      snyk
+    }
+  }
 })
 </script>
 
 <template>
 <div>
-<p>Here is the response from devTo: {{ data }}</p>
+<p>Here is the response from devTo: {{ devTo }}</p>
+<br>
+<p> ************************************************</p>
+<p>Here is the response from Snyk Vulns: {{ snyk }} </p>
 </div>
 </template>
 
