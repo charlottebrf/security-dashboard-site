0fdcb1fad5655d1def8d28e28246e01883aeba3a
Charlotte Fereday <cbrfereday@gmail.com>
2022-02-27 18:36:08 +0000

    feat: add in some head metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,16 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    titleTemplate: 'Security News Dashboard',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+      // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+      { hid: 'Security News Dashboard', name: 'Security News Dashboard', content: 'Meta description' }
+    ]
+  }, 
     "app": {
         "basePath": "/",
         "assetsPath": "/_nuxt/",
