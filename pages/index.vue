f70ee4ac614b2723212212561686f7c71956e61b
Charlotte Fereday <cbrfereday@gmail.com>
2022-02-27 18:36:32 +0000

    feat: update the index page to reference the new pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,16 @@
 <script>
-import Card from '../components/Card.vue'
-export default defineComponent({
-  async setup() {   
-    const [{ data: devTo}, { data: snyk }] = await Promise.all([
-      useFetch(`https://dev.to/api/articles?per_page=5&tag=security`),
-       useFetch(`https://snyk.io/disclosed-vulnerabilities/data`, { method: 'POST', headers: {'x-requested-with': 'XMLHttpRequest' }}),
-    ])
-    return {
-      devTo,
-      snyk
-    }
-  },
-  data() {
-      return {
-        devToArticles: [],
-        snykArticles: [],
-      }
-  },
-  methods: {
-    goOverStuff(articles) {
-        const changed = articles.map((article) => {
-        return { title: article.title, description: article.description, url: article.url }
-    })
-    return changed;
-    },
-    goOverStuffOther(articles) {
-        let changed = []
-        for (const article of articles.data.vulns) {
-            changed.push({ title: article.title, description: article.id, url: `https://security.snyk.io/vuln/${article.id}`})
-        }
-    return changed;
-    }
-},
-created() {
-    this.devToArticles = this.goOverStuff(this.devTo),
-    this.snykArticles = this.goOverStuffOther(this.snyk)
-},
-})
 </script>
 
 <template>
-<div class="wrapper">
-<Card
-    v-for="article in devToArticles"
-    :key="article.title" 
-    :title="article.title"
-    :description="article.description"
-    :url="article.url"
->
-</Card>
-<Card
-    v-for="article in snykArticles"
-    :key="article.description" 
-    :title="article.title"
-    :description="article.description"
-    :url="article.url"
->
-</Card>
-</div>
+<div>
+  <h1>Security News Dashboard</h1>
+  <NuxtLink to="/snykVulns">Find out about the latest Snyk Vulns</NuxtLink>
+  <br/>
+  <br/>
+  <NuxtLink to="/devTo">Read the latest DevTo Articles</NuxtLink>
+  </div>
 </template>
 
 <style>
-.wrapper {
-    display: flex; /* or inline-flex */
-    row-gap: 10px;
-    column-gap: 20px;
-    flex-direction: row;
-    flex-wrap: wrap;
-}
 </style>
 
